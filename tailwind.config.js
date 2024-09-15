1a7923399c5162b066aba06e57629268332df308
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-15 23:40:32 +0200

    tailwind installation and setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Add paths for JS/TS/JSX/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
