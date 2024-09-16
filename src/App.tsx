e2cee2773c63cd1a720d0d2d6c1788b9fc7bc383
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-16 23:33:56 +0200

    App.tsx fetchData, app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchUsersData } from "./features/usersData/usersDataSlice"
+import { AppDispatch, RootState } from "./store"
+import { FiltersContainer } from "./components/FiltersContainer"
+import { Table } from "./components/Table"
+
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const {usersData, loading, error } = useSelector((state: RootState) => state.usersData);
+
+  useEffect(()=>{
+    dispatch(fetchUsersData());
+  }, []);
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+
+  if(error){
+    return <h1>Error: {error}</h1>
+  }
 
   return (
-    <div className="bg-blue-500 text-white text-center p-4">
-      <h1 className="text-4xl font-bold">Tailwind</h1>
+    <div className="bg-background min-h-screen flex items-center flex-col">
+      <div className="w-fit">
+        <FiltersContainer/>
+        <Table usersData={usersData}/>
+      </div>
     </div>
   )
 }
