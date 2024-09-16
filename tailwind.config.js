61fac3026cd6ea1b36637a1a54e66e65a37a4846
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-16 23:31:14 +0200

    tailwind.config.js add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}', // Add paths for JS/TS/JSX/TSX files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#dee2e6',
+        'main': '#014f86',
+        'secondary': '#014f86',
+      }
+    },
   },
   plugins: [],
 }
