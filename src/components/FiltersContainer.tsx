92a96c58367f0828be022bdffad3ec186dca85a1
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-17 13:25:32 +0200

    FiltersContainer.tsx pass field to each FilterInput component

--- src/components/FiltersContainer.tsx
+++ src/components/FiltersContainer.tsx
@@ -3,10 +3,10 @@ import { FilterInput } from "./FilterInput"
 export const FiltersContainer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-10 gap-y-5 h-fit bg-main p-5 w-full drop-shadow-md mt-5">
-      <FilterInput id="name" text="Name" placeholder="Name"/>
-      <FilterInput id="username" text="Username" placeholder="Username"/>
-      <FilterInput id="email" text="E-mail" placeholder="E-mail"/>
-      <FilterInput id="phone" text="Phone number" placeholder="Phone number"/>
+      <FilterInput field="name" id="name" text="Name" placeholder="Name"/>
+      <FilterInput field="username" id="username" text="Username" placeholder="Username"/>
+      <FilterInput field="email" id="email" text="E-mail" placeholder="E-mail"/>
+      <FilterInput field="phone" id="phone" text="Phone number" placeholder="Phone number"/>
     </div>
   )
 }
