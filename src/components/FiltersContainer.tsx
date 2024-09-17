78e00aa6eaf473026cc657295c4b8bdc03b38552
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-17 19:57:25 +0200

    FiltersContainer.tsx bg color change

--- src/components/FiltersContainer.tsx
+++ src/components/FiltersContainer.tsx
@@ -2,7 +2,7 @@ import { FilterInput } from "./FilterInput"
 
 export const FiltersContainer = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-10 gap-y-5 h-fit bg-main p-5 w-full drop-shadow-md mt-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-10 gap-y-5 h-fit bg-primary p-5 w-full drop-shadow-md mt-5 mb-3">
       <FilterInput field="name" id="name" text="Name" placeholder="Name"/>
       <FilterInput field="username" id="username" text="Username" placeholder="Username"/>
       <FilterInput field="email" id="email" text="E-mail" placeholder="E-mail"/>
