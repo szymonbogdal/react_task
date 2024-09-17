84bb92425db23f941c41fa3ff941dc94ade6c3e0
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-17 13:26:07 +0200

    FiltersInput.tsx dispatch filters on input debounce change

--- src/components/FilterInput.tsx
+++ src/components/FilterInput.tsx
@@ -1,10 +1,29 @@
+import { useDispatch } from "react-redux"
+import { setFilter } from "../features/filters/filtersSlice"
+import { useCallback } from "react";
+import { debounce } from "../utils/debounce";
+
 type FilterProps = {
+  field: 'name' | 'username' | 'email' | 'phone';
   id: string;
   text: string;
   placeholder: string;
 }
 
-export const FilterInput = ({id, text, placeholder}: FilterProps) => {
+export const FilterInput = ({id, text, placeholder, field}: FilterProps) => {
+
+  const dispatch = useDispatch();
+
+  const handleDebouncedChange = useCallback(
+    debounce((value: string) => {
+      dispatch(setFilter({ field: field, value }));
+    }, 300),
+    [dispatch, field]
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleDebouncedChange(e.target.value);
+  };
 
   return (
     <div className="flex flex-col space-y-1">
@@ -17,6 +36,7 @@ export const FilterInput = ({id, text, placeholder}: FilterProps) => {
         className="bg-background px-2 py-0.5 rounded-lg focus:outline-secondary"
         placeholder={placeholder}
         autoComplete="off"
+        onChange={handleChange}
       />
     </div>
   )
